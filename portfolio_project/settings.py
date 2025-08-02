a7625c8d658746755714e8b8721ecfffdf058f69
Sharad Shinde <shindesharad9325@gmail.com>
2025-08-02 16:14:50 +0530

    Configure whitenoise and static root

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# ADD THIS LINE: This tells Django where to collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
